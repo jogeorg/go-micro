d70ec015b76531d6553b5b1ecfe5b741bbfd2ef2
John <jogeorg@microsoft.com>
2023-09-05 11:33:19 -0400

    add gRPC service

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -52,6 +52,8 @@ func main() {
 	err = rpc.Register(new(RPCServer))
 	go app.rpcListen()
 
+	go app.grpcListen()
+
 	srv := &http.Server{
 		Addr: fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
